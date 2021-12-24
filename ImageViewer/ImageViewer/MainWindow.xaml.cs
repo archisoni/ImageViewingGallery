a9280775ba02bde3c8a10a094119739abaa77a7c
Archi Soni <64640027+archisoni@users.noreply.github.com>
2021-12-24 14:17:33 +0530

    Update MainWindow.xaml.cs

--- ImageViewer/ImageViewer/MainWindow.xaml.cs
+++ ImageViewer/ImageViewer/MainWindow.xaml.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using MahApps.Metro.Controls;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
